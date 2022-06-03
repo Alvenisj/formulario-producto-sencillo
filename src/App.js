a0ac1d9c9bd3999e9fa2b5ecae36d4b65b9fd8f7
Alvenisj <alvenisj@gmail.com>
2022-06-03 18:05:15 -0500

    proyecto sencillo con javascript agregando product y eliminando producto

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { Product } from "./Product.js";
+import { UI } from "./UI.js";
+
+// DOM Events
+const productForm = document.getElementById("product-form");
+
+  productForm.addEventListener("submit",  (e) => {
+    // Override the default Form behaviour
+    e.preventDefault();
+
+    // Getting Form Values
+    const name = document.getElementById("name").value;
+    const price = document.getElementById("price").value;
+    const year = document.getElementById("year").value;
+
+    // Create a new Oject Product
+    const product = new Product(name, price, year);
+
+    // Create a new UI instance
+    const ui = new UI();
+
+    // Input User Validation
+    if (name === "" || price === "" || year === "") {
+      ui.showMessage("Please Insert data in all fields", "danger");
+
+    }
+
+
+
+      // Save Product
+      ui.addProduct(product);
+      ui.showMessage("Product Added Successfully", "success");
+      ui.resetForm();
+
+ 
+  });
+
+document.getElementById("product-list").addEventListener("click", (e) => {
+  const ui = new UI();
+  ui.deleteProduct(e.target);
+  e.preventDefault();
+});
